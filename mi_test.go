655d4a972996c2117a8e5ff4f6cea50a2975f640
xpinguin <xpinguin@users.noreply.github.com>
2019-07-01 22:21:41 +0300

    non-relevant parts removed from the 'run' function; focus is on ssa/interp basic functioning test

--- mi_test.go
+++ mi_test.go
@@ -10,7 +10,6 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
-	"time"
 
 	"golang.org/x/tools/go/loader"
 	"golang.org/x/tools/go/ssa/interp"
@@ -29,37 +28,16 @@ import (
 /////////////////////////////
 //////////////////////////// (FROM ssa.interp/interp_test.go)
 func run(t *testing.T, input string) bool {
-	t.Logf("Input: %s\n", input)
-
-	start := time.Now()
-
-	ctx := build.Default    // copy
-	ctx.GOROOT = "testdata" // fake goroot
-	/*ctx.GOOS = "linux"
-	ctx.GOARCH = "amd64"*/
+	////
+	ctx := build.Default
+	ctx.GOROOT = "testdata"
 
 	conf := loader.Config{Build: &ctx}
 	if _, err := conf.FromArgs([]string{input}, true); err != nil {
 		t.Errorf("FromArgs(%s) failed: %s", input, err)
 		return false
 	}
-
 	conf.Import("runtime")
-
-	// Print a helpful hint if we don't make it to the end.
-	var hint string
-	defer func() {
-		if hint != "" {
-			fmt.Println("FAIL")
-			fmt.Println(hint)
-		} else {
-			fmt.Println("PASS")
-		}
-
-		interp.CapturedOutput = nil
-	}()
-
-	hint = fmt.Sprintf("To dump SSA representation, run:\n%% go build golang.org/x/tools/cmd/ssadump && ./ssadump -test -build=CFP %s\n", input)
 
 	iprog, err := conf.Load()
 	if err != nil {
@@ -67,6 +45,7 @@ func run(t *testing.T, input string) bool {
 		return false
 	}
 
+	////
 	prog := ssautil.CreateProgram(iprog, ssa.SanityCheckFunctions)
 	prog.Build()
 
@@ -75,24 +54,22 @@ func run(t *testing.T, input string) bool {
 		t.Fatalf("not a main package: %s", input)
 	}
 
+	////
 	interp.CapturedOutput = new(bytes.Buffer)
+	defer func() {
+		interp.CapturedOutput = nil
+	}()
 
-	hint = fmt.Sprintf("To trace execution, run:\n%% go build golang.org/x/tools/cmd/ssadump && ./ssadump -build=C -test -run --interp=T %s\n", input)
-	exitCode := interp.Interpret(mainPkg, 0, &types.StdSizes{WordSize: 8, MaxAlign: 8}, input, []string{})
+	exitCode := interp.Interpret(
+		mainPkg, 0,
+		&types.StdSizes{WordSize: 8, MaxAlign: 8},
+		input,
+		[]string{})
+
 	if exitCode != 0 {
 		t.Fatalf("interpreting %s: exit code was %d", input, exitCode)
+		return false
 	}
-	// $GOROOT/test tests use this convention:
-	if strings.Contains(interp.CapturedOutput.String(), "BUG") {
-		t.Fatalf("interpreting %s: exited zero but output contained 'BUG'", input)
-	}
-
-	hint = "" // call off the hounds
-
-	if false {
-		t.Log(input, time.Since(start)) // test profiling
-	}
-
 	return true
 }
 
