ea54423c6365e66a79317b7806e60833a6c8bdcc
xpinguin <xpinguin@users.noreply.github.com>
2019-07-07 15:22:21 +0300

    ast repr in prolog lists and atoms

--- ast_test.go
+++ ast_test.go
@@ -5,50 +5,77 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"reflect"
 	"strings"
 	"testing"
 
-	"github.com/xpinguin/golog-mi/ext/go/ast2"
+	"github.com/mndrix/golog/term"
+
+	"github.com/iancoleman/strcase"
 )
 
 func TestInspectAST(t *testing.T) {
 	fset := token.NewFileSet()
-	f, _ := parser.ParseFile(fset, "testdata/squares.go", nil, 0 /*parser.Trace | parser.ParseComments*/)
+	f, _ := parser.ParseFile(fset, "golog_bridge.go" /*"testdata/squares.go"*/, nil, 0 /*parser.Trace | parser.ParseComments*/)
 
-	//inspect := inspector.New([]*ast.File{f})
+	/*env := map[ast.Node]*ast.Object{}
+	ns0 := map[string]*ast.Object{}
+	ns := map[string]ast.Node{}*/
+
+	depth := 0 // increase - [, decrease - ]
+	ctr := 0   // preorder counter (nil's are ignored)
+	trail := []string{}
+
 	ast.Inspect(f, func(n ast.Node) bool {
 		if n == nil {
-			return false
-		}
-		switch x := n.(type) {
-		case *ast.File:
-			return true
-
-		case *ast.TypeSpec:
-			//spew.Dump(x)
-		////////////////
-		case ast.Spec:
-
-		case *ast.FuncDecl:
-			if id := x.Name; id != nil {
-				if strings.HasPrefix(id.Name, "Square") {
-					ast2.Print(fset, n)
-					//spew.Dump(x)
-				}
+			depth--
+			if strings.TrimSpace(trail[len(trail)-1]) == "," {
+				trail = append(trail, trail[len(trail)-1])
+				trail[len(trail)-2] = "]"
 			}
-		////////////////
-		case ast.Decl:
-			//spew.Dump(x)
-
-		case ast.Stmt:
-		case ast.Expr:
-		default:
-			fmt.Println("\n==========\n==========")
-			//ast2.Print(fset, n)
-			fmt.Println("-------------")
-			//spew.Dump(x)
-			fmt.Print("=====================.\n\n")
+			return true
 		}
-		return false // XXX: temporary search-space limiter
+
+		var headTerm Term
+
+		astNodeTermName := func(n ast.Node) string {
+			nData := reflect.Indirect(reflect.ValueOf(n))
+			nName := nData.Type().Name()
+			return strcase.ToSnake(nName)
+		}
+
+		switch x := n.(type) {
+		case *ast.BinaryExpr:
+			headTerm = term.NewAtom(x.Op.String())
+		case *ast.UnaryExpr:
+			headTerm = term.NewAtom(x.Op.String())
+		case *ast.StarExpr:
+			headTerm = term.NewAtom("*")
+		case *ast.AssignStmt:
+			headTerm = Fntr_(astNodeTermName(x), x.Tok.String())
+		case *ast.BasicLit:
+			headTerm = Fntr_(
+				strcase.ToSnake(x.Kind.String()),
+				strings.Trim(x.Value, `"' `))
+		case *ast.Ident:
+			headTerm = Fntr_("id", x.Name)
+			if x.Obj != nil {
+				headTerm = Fntr_("ref", x.Obj.Name, headTerm)
+			}
+		default:
+			headTerm = term.NewAtom(astNodeTermName(n))
+		}
+
+		depth++
+		trail = append(trail, "[", headTerm.String(), ", ")
+		ctr++
+		return true
 	})
+	if len(trail) > 0 && strings.TrimSpace(trail[len(trail)-1]) == "," {
+		trail[len(trail)-1] = "."
+	}
+
+	fmt.Printf("%v\n", strings.Join(trail, ""))
+	fmt.Printf("\nD: %d ;; CTR: %d\n\n", depth, ctr)
+	//spew.Dump(trace)
 }
