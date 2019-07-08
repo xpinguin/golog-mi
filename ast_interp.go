64db5d79c650e6c6f9e2bf92091d1b499e203097
xpinguin <xpinguin@users.noreply.github.com>
2019-07-08 08:05:42 +0300

    generate proper SWI-prolog modules for the each ast.File

--- ast_interp.go
+++ ast_interp.go
@@ -1,14 +1,19 @@
 package mi
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"io/ioutil"
+	"os"
 	"reflect"
 	"strings"
+	"time"
 
 	"github.com/iancoleman/strcase"
 	"github.com/mndrix/golog/term"
+	"github.com/xpinguin/golog-mi/util"
 )
 
 func FileToTerms(f *ast.File, fset *token.FileSet) (termsTrail []string) {
@@ -78,17 +83,62 @@ func FileToTerms(f *ast.File, fset *token.FileSet) (termsTrail []string) {
 		ctr++
 		return true
 	})
-	if len(trail) > 0 && strings.TrimSpace(trail[len(trail)-1]) == "," {
+	/*if len(trail) > 0 && strings.TrimSpace(trail[len(trail)-1]) == "," {
 		trail[len(trail)-1] = "."
-	}
+	}*/
 
 	//fmt.Printf("\nD: %d ;; CTR: %d\n\n", depth, ctr)
 	return trail
 }
 
-func ParseToTerms(file string) (termsTrail []string) {
+func ParseFileToTerms(file string) (termsTrail []string) {
 	fset := token.NewFileSet()
 	f, _ := parser.ParseFile(fset, file, nil, 0 /*parser.Trace | parser.ParseComments*/)
 
 	return FileToTerms(f, fset)
+}
+
+func ProgramToTerms(mainPkg string, outDir string) (fileTermsTrails map[string][]string) {
+	fileTermsTrails = map[string][]string{}
+
+	prg := util.LoadProgram(mainPkg)
+	fset := prg.Fset
+
+	for _, pkgInfo := range prg.AllPackages {
+		////////////////
+		if pkgPath := pkgInfo.Pkg.Path(); strings.Contains(pkgPath, "vendor") {
+			continue
+		}
+		////////////////
+		for _, f := range pkgInfo.Files {
+			var name string
+			if f.Name != nil {
+				name = f.Name.Name
+			} else {
+				// failsafe naming :P
+				name = fmt.Sprintf("%#v", f.Pos())
+			}
+			if _, ok := fileTermsTrails[name]; ok {
+				name += "_" + time.Now().String()
+			}
+			fileTermsTrails[name] = FileToTerms(f, fset)
+		}
+	}
+
+	for fname, trail := range fileTermsTrails {
+		mname := term.NewAtom(fname).String()
+		preamble := []string{
+			`:- encoding(utf8).` + "\n",
+			`:- module(` + mname + `, [program/2]).` + "\n\n",
+		}
+		trail = append(preamble, trail...)
+		ioutil.WriteFile(
+			strings.TrimRight(outDir, "/")+"/"+fname+".pl",
+			[]byte(
+				"program("+mname+",\n"+
+					strings.Join(trail, "")+
+					"\n)."),
+			os.ModePerm)
+	}
+	return fileTermsTrails
 }
